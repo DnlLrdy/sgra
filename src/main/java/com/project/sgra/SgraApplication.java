98f5f6e490fcf7be547822c305d6b9efe1a4afb4
DnlLrdy <lndplfydrlcrg@gmail.com>
2025-04-15 10:10:37 -0500

    subiendo el proyecto a github

---
+++ src/main/java/com/project/sgra/SgraApplication.java
@@ -0,0 +1,13 @@
+package com.project.sgra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sgra/SgraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sgra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
