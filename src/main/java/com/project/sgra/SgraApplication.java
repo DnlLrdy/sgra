464ab148c35b183d6f9da3f0b653f507e95980ea
DnlLrdy <lndplfydrlcrg@gmail.com>
2025-04-15 10:54:55 -0500

    agregando la clase de seguridad y la conexion a la base de datos

--- src/main/java/com/project/sgra/SgraApplication.java
+++ src/main/java/com/project/sgra/SgraApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SgraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SgraApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SgraApplication.class, args);
+    }
 
 }
