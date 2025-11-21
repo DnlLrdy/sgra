cde9c62e717d0cc8c0ef30e4ab9cec79941da9c9
DnlLrdy <lndplfydrlcrg@gmail.com>
2025-11-21 18:29:21 -0500

    Se modifico el diseño de el modulo predictivo.

--- src/main/java/com/project/sgra/controller/AdminPredictionController.java
+++ src/main/java/com/project/sgra/controller/AdminPredictionController.java
@@ -1,6 +1,8 @@
 package com.project.sgra.controller;
 
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,13 @@ public class AdminPredictionController {
 
     @GetMapping("/prediccion")
     public String prediccion(Model model) {
-        // puedes añadir atributos al modelo si necesitas (por ejemplo, opciones para selects)
+        model.addAttribute("administradorNombreUsuario", administradorNombreUsuario());
         return "admin/prediccion/predict"; // -> src/main/resources/templates/admin/prediccion/predict.html
     }
+
+    public String administradorNombreUsuario() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
+    }
+
 }
