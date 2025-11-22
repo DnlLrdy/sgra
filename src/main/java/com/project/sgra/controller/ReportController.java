e4c67bb691c90761cc1aa9a7d9ce79e634226888
raulinhob <raulandresb21@gmail.com>
2025-11-21 22:35:04 -0500

    Mejoras de Modal en modulo predictivo

--- src/main/java/com/project/sgra/controller/ReportController.java
+++ src/main/java/com/project/sgra/controller/ReportController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/reports")
+@RequestMapping("api/reports")
 @CrossOrigin(origins = "*")
 public class ReportController {
 
--- src/main/java/com/project/sgra/model/Report.java
+++ src/main/java/com/project/sgra/model/Report.java
@@ -1,52 +1,36 @@
 package com.project.sgra.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
 @Document(collection = "reports")
 public class Report {
 
     @Id
     private String id;
 
-    private String timestamp; // fecha ISO
+    private String timestamp;
+
     private double mileage;
     private String maintenanceHistory;
-    private String prediction;
-    private String accuracy;
-    private int vehicleAge;
+    private double reportedIssues;
+    private double vehicleAge;
     private String fuelType;
     private String transmissionType;
-    private int reportedIssues;
 
-    // Getters y Setters
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    private double engineSize;
+    private double lastServiceDate;
+    private double serviceHistory;
+    private double accidentHistory;
+    private double fuelEfficiency;
 
-    public String getTimestamp() { return timestamp; }
-    public void setTimestamp(String timestamp) { this.timestamp = timestamp; }
+    private String tireCondition;
+    private String brakeCondition;
+    private String batteryStatus;
 
-    public double getMileage() { return mileage; }
-    public void setMileage(double mileage) { this.mileage = mileage; }
+    private String prediction;
+    private String accuracy;
 
-    public String getMaintenanceHistory() { return maintenanceHistory; }
-    public void setMaintenanceHistory(String maintenanceHistory) { this.maintenanceHistory = maintenanceHistory; }
-
-    public String getPrediction() { return prediction; }
-    public void setPrediction(String prediction) { this.prediction = prediction; }
-
-    public String getAccuracy() { return accuracy; }
-    public void setAccuracy(String accuracy) { this.accuracy = accuracy; }
-
-    public int getVehicleAge() { return vehicleAge; }
-    public void setVehicleAge(int vehicleAge) { this.vehicleAge = vehicleAge; }
-
-    public String getFuelType() { return fuelType; }
-    public void setFuelType(String fuelType) { this.fuelType = fuelType; }
-
-    public String getTransmissionType() { return transmissionType; }
-    public void setTransmissionType(String transmissionType) { this.transmissionType = transmissionType; }
-
-    public int getReportedIssues() { return reportedIssues; }
-    public void setReportedIssues(int reportedIssues) { this.reportedIssues = reportedIssues; }
 }
